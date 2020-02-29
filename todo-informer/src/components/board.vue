87ce5d354382eb797317959ffbff8ed58ae4d39e
KI-RA-CO <kihara4173@icloud.com>
2020-02-29 20:32:06 +0900

    remove initial page's code

---
+++ todo-informer/src/components/board.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    <header>
+      my Trello
+    </header>
+    <mian>
+      <p class="info-line">All: 0 tasks</p>
+    </mian>
+  </div>
+</template>
+
+<script>
+export default {
+  
+}
+</script>
