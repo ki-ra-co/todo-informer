1be778ac005570adbc2b11efd31afce78bc716fb
KI-RA-CO <kihara4173@icloud.com>
2020-02-29 22:46:44 +0900

    edit code on store

--- todo-informer/src/store/index.js
+++ todo-informer/src/store/index.js
@@ -2,14 +2,44 @@ import Vue from 'vue'
 import Vuex from 'vuex'
 
 Vue.use(Vuex)
+const savedLists = localStorage.getItem('trello-lists')
 
-export default new Vuex.Store({
+const store = new Vuex.Store({
   state: {
+    lists: savedLists ? JSON.parse(savedLists): [
+      {
+        title: 'Backlog',
+        cards: [
+          { body: 'English' },
+          { body: 'Mathematics' },
+        ]
+      },
+      {
+        title: 'Todo',
+        cards: [
+          { body: 'Science' }
+        ]
+      },
+      {
+        title: 'Doing',
+        cards: []
+      }
+    ],
   },
   mutations: {
+    addlist(state, payload) {
+      state.lists.push({ title: payload.title, cards:[] })
+    },
   },
   actions: {
-  },
-  modules: {
+    addlist(context, payload) {
+      context.commit('addlist', payload)
+    },
   }
 })
+
+store.subscribe((mutation, state) => {
+  localStorage.setItem('trello-lists', JSON.stringify(state.lists))
+})
+
+export default store
